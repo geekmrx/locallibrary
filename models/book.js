3676817493aea9c3dad9b237adf8ca7182071e8d
ClauberDev <clauber.dev@gmail.com>
2023-06-27 20:52:25 -0300

    cometários adicionado

--- models/book.js
+++ models/book.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const BookSchema = new Schema({
   title: { type: String, required: true },
@@ -14,7 +14,9 @@ const BookSchema = new Schema({
   genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }]
 })
 
+// Virtual for book's URL
 BookSchema.virtual("url").get(function() {
+  // We don't use an arrow function as we'll need the this object
   return `/catalog/book/${this._id}`
 })
 
