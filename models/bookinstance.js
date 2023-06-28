bb63c65f41c504603d966f6803adb6ac523af551
ClauberDev <clauber.dev@gmail.com>
2023-06-27 21:15:26 -0300

    Types removido

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const BookInstanceSchema = new Schema({
-    book: { type: Schema.Types.ObjectId, ref: "Book", required: true }, // reference to the associated book
+    book: { type: Schema.ObjectId, ref: "Book", required: true }, // reference to the associated book
     imprint: { type: String, required: true },
     status: {
         type: String,
